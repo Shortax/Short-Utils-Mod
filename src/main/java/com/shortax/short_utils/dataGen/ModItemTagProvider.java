23e3e4b591ad5afa794fcdda57062d8394832d7c
Shortax <63542171+Shortax@users.noreply.github.com>
2025-05-25 12:00:37 +0200

    licenses and removal of assets

--- src/main/java/com/shortax/short_utils/dataGen/ModItemTagProvider.java
+++ src/main/java/com/shortax/short_utils/dataGen/ModItemTagProvider.java
@@ -1,3 +1,24 @@
+/*
+ *
+ *  * This file is part of Short Utils mod
+ *  *
+ *  * Copyright (C) 2025 Shortax
+ *  *
+ *  * This program is free software: you can redistribute it and/or modify
+ *  * it under the terms of the GNU Lesser General Public License as published by
+ *  * the Free Software Foundation, either version 3 of the License, or
+ *  * (at your option) any later version.
+ *  *
+ *  * This program is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  * GNU Lesser General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU Lesser General Public License
+ *  * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 package com.shortax.short_utils.dataGen;
 
 import com.shortax.short_utils.Initializers.ModBlocks;
@@ -26,6 +47,8 @@ public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
                         Block::asItem), //->
                 getOrCreateTagBuilder(ModTags.Items.COL_RED_LAMP_ITEM)::add
         );
+
+        getOrCreateTagBuilder(ModTags.Items.CUSTOM_PRESS_PLATES).add(ModBlocks.OBSID_PRESSURE_PLATE.asItem());
 
         getOrCreateTagBuilder(ModTags.Items.FAKE_REDSTONE_BLOCKS).add(ModBlocks.FAKE_OAK_TRAPDOOR.asItem());
         getOrCreateTagBuilder(ModTags.Items.FAKE_REDSTONE_BLOCKS).add(ModBlocks.FAKE_SPRUCE_TRAPDOOR.asItem());
